f4943ec42422de9ccade6a483aa89c0d83e0c761
ngocannguyen02 <scrapnews514@gmail.com>
2016-11-20 09:02:32 -0500

    clean

--- README.md
+++
@@ -1,8 +0,0 @@
-# back-end
-
-URL : Sample-env.8tnhkguxtj.us-west-2.elasticbeanstalk.com 
-
-Same key ScrapNewsServer.pem
-
-SSH
-ec2-user@ec2-35-162-181-23.us-west-2.compute.amazonaws.com
--- src/main/java/net/dontdrinkandroot/example/angularrestspringsecurity/entity/Entity.java
+++
@@ -1,8 +0,0 @@
-package net.dontdrinkandroot.example.angularrestspringsecurity.entity;
-
-import java.io.Serializable;
-
-public interface Entity extends Serializable
-{
-    Long getId();
-}
